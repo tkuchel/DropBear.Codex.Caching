2e275dbb589b65c93f3c34aa11662c3fb6e4236c
DropBear <tkuchel@users.noreply.github.com>
2024-03-21 16:10:01 +1100

    Removed ILogger use, updated to async versions of methods.

--- DropBear.Codex.Caching/Triggers/CachePreloaderTrigger.cs
+++ DropBear.Codex.Caching/Triggers/CachePreloaderTrigger.cs
@@ -1,8 +1,6 @@
-using Cysharp.Text;
 using DropBear.Codex.Caching.Interfaces;
 using DropBear.Codex.Caching.State;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace DropBear.Codex.Caching.Triggers;
 
@@ -11,40 +9,34 @@ public static class CachePreloaderTrigger
     public static async Task TriggerPreloading(IServiceProvider serviceProvider)
     {
         // Create a scope for resolving cache providers
-        using var scope = serviceProvider.CreateScope();
-        var logger = scope.ServiceProvider.GetService<ILogger>();
-
-        // Check if preloading has already occured
-        if (PreloadingState.PreloadingExecuted)
+        var scope = serviceProvider.CreateAsyncScope();
+        // Create a scope for resolving cache providers
+        await using (scope.ConfigureAwait(false))
         {
-            logger?.LogInformation("Cache preloading has already been executed, skipping.");
-            return; // Preloading has already been executed, skip.
+            // Check if preloading has already occured
+            if (PreloadingState.PreloadingExecuted) return; // Preloading has already been executed, skip.
+
+            // Resolve all cache preloaders and logger
+            var preloaders = scope.ServiceProvider.GetServices<ICachePreloader>();
+
+
+            // Preloader registration check
+            var cachePreloaders = preloaders as ICachePreloader[] ?? preloaders.ToArray();
+            if (cachePreloaders.Length is 0) return;
+
+            // Process each preloader
+            foreach (var preloader in cachePreloaders)
+                try
+                {
+                    await preloader.PreloadAsync().ConfigureAwait(false);
+                    PreloadingState.PreloadingExecuted = true;
+                }
+#pragma warning disable CS0168 // Variable is declared but never used
+                catch (Exception ex)
+#pragma warning restore CS0168 // Variable is declared but never used
+                {
+                    PreloadingState.PreloadingExecuted = false;
+                }
         }
-
-        // Resolve all cache preloaders and logger
-        var preloaders = scope.ServiceProvider.GetServices<ICachePreloader>();
-
-
-        // Preloader registration check
-        var cachePreloaders = preloaders as ICachePreloader[] ?? preloaders.ToArray();
-        if (cachePreloaders.Length is 0)
-        {
-            logger?.LogInformation("No cache preloaders found.");
-            return;
-        }
-
-        // Process each preloader
-        foreach (var preloader in cachePreloaders)
-            try
-            {
-                await preloader.PreloadAsync().ConfigureAwait(false);
-                PreloadingState.PreloadingExecuted = true;
-                logger?.LogInformation(ZString.Format("Cache preloading executed for {0}.", preloader.GetType().Name));
-            }
-            catch (Exception ex)
-            {
-                PreloadingState.PreloadingExecuted = false;
-                logger?.LogError(ex, ZString.Format("Cache preloading failed for {0}.", preloader.GetType().Name));
-            }
     }
 }
